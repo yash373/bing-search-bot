185257c98cd1d1c27db62fb5a60b8021791e7037
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2025-08-10 19:48:45 +0530

    got random search setup

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import time
 import keyboard
+import random
+import nltk
+import os
+from nltk.corpus import words
+from nltk.corpus import words
+
+def get_random_words():
+    nltk.download('words', quiet=True)
+    word_list = words.words()
+    return ' '.join(random.sample(word_list, 4))
 
 def make_search():
     print("search content")
+
+print(get_random_words())
